498d71bacb827243a92d8fa8d751aee41ff479fc
mahdi bashiri <mahdi94250@gmail.com>
2023-01-10 17:42:53 +0330

    litle edited class

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
         </div>
         {/*options*/}
         <div className="h-full flex items-center gap-x-5">
-          <Link to="/likes" className="hidden relative hover:bg-blue-200 hover:border-blue-200 duration-300 w-11 h-11 lg:flex items-center justify-center rounded-full border-2">
+          <Link to="/likes" className="relative hover:bg-blue-200 hover:border-blue-200 duration-300 w-11 h-11 flex items-center justify-center rounded-full border-2">
             <img src="/icons/Likes.svg" alt="icon-Likes" />
             {likeState.length > 0 && <span className="bg-red-500 -top-1.5 -right-1.5 rounded-full text-[12px] w-5 h-5 flex items-center justify-center absolute text-white ">{likeState.length}</span>}
           </Link>
