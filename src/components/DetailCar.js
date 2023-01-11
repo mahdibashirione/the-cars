97ca518233c8c2fb599c5392fcf44f7581e5f9ab
mahdi bashiri <mahdi94250@gmail.com>
2023-01-11 17:09:44 +0330

    add notive like/unlike to app

--- src/components/DetailCar.js
+++ src/components/DetailCar.js
@@ -2,22 +2,45 @@ import { Link } from "react-router-dom";
 import { FcLike } from "react-icons/fc";
 import { FiHeart } from "react-icons/fi";
 import { useLike, useLikeDispatch } from "../context/likes/LikesProvider";
+import { forwardRef, useState } from "react";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const DetailCar = ({ detail }) => {
 
   const likeState = useLike()
   const likeDispatch = useLikeDispatch()
+  const [openSuccess, setOpenSuccess] = useState(false);
+  const [openError, setOpenError] = useState(false);
 
   const isLike = likeState.findIndex(car => car.id === detail.id)
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenSuccess(false)
+    setOpenError(false)
+  };
 
   return (
     <div className="w-full">
       <div className="w-full flex justify-between mb-4 lg:mb-9">
         <h2 className="text-xl lg:text-2xl font-bold select-none">{detail.name}</h2>
-        {isLike >= 0 ? <button onClick={() => likeDispatch({ type: "Un_Like", payload: detail })} >
+        {isLike >= 0 ? <button onClick={() => {
+          likeDispatch({ type: "Un_Like", payload: detail })
+          setOpenError(true)
+        }} >
           <FcLike className="text-2xl mb-2" />
         </button> :
-          <button onClick={() => likeDispatch({ type: "Like", payload: detail })} >
+          <button onClick={() => {
+            likeDispatch({ type: "Like", payload: detail })
+            setOpenSuccess(true)
+          }} >
             <FiHeart className="text-2xl text-gray-400 mb-2" />
           </button>}
       </div>
@@ -48,6 +71,26 @@ const DetailCar = ({ detail }) => {
         </div>
         <Link to="/" className="hover:scale-95 outline-none duration-300 text-[12px] py-3 px-4 rounded lg:text-base text-white bg-blue-500">Rent Now</Link>
       </div>
+      <Snackbar
+        open={openSuccess}
+        autoHideDuration={1000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          Liked Car
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={openError}
+        autoHideDuration={1000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          unLiked Car
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
