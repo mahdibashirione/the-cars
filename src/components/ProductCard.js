340382c34bec0d29af42b0c7113ceb12922d6fea
mahdi bashiri <mahdi94250@gmail.com>
2023-01-19 20:27:36 +0330

    fixed page search

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -11,9 +11,9 @@ const ProductCard = ({ data }) => {
   const isLike = likeState.findIndex(car => car.id === data.id)
 
   return (
-    <div className="bg-white dark:bg-zinc-800 shadow flex flex-col items-center rounded-[10px] pt-[19px] p-4 col-span-1 lg:w-auto mb-8 lg:mb-10">
+    <div className="bg-white dark:bg-zinc-800 shadow flex flex-col items-center rounded-[10px] pt-[19px] p-4 col-span-1 lg:w-auto">
       <div className="w-full flex items-center justify-between">
-        <span className="lg:text-xl dark:text-white">{data.name}</span>
+        <span className="lg:text-xl dark:text-white select-none">{data.name}</span>
         {isLike >= 0 ? <button onClick={() => likeDispatch({ type: "Un_Like", payload: data })} >
           <FcLike className="text-2xl mb-2" />
         </button> :
@@ -28,22 +28,20 @@ const ProductCard = ({ data }) => {
       </Link>
       <div className="w-full flex lg:mt-4 items-center justify-between select-none mb-7 lg:mb-5">
         <div className="flex items-center gap-x-1">
-          <div className="w-4 lg:w-6 h-4 lg:h-6"><img className="w-full object-cover" src="/icons/Gas.svg" alt="icon-gas-station" /></div>
+          <div className="w-4 lg:w-5 h-4 lg:h-5"><img className="w-full object-cover" src="/icons/Gas.svg" alt="icon-gas-station" /></div>
           <span className="text-gray-500 text-[12px] lg:text-sm">{data.liter}L</span>
         </div>
         <div className="flex items-center gap-x-1">
-          <div className="w-4 lg:w-6 h-4 lg:h-6"><img className="w-full object-cover" src="/icons/Manual.svg" alt="icon-Manual" /></div>
+          <div className="w-4 lg:w-5 h-4 lg:h-5"><img className="w-full object-cover" src="/icons/Manual.svg" alt="icon-Manual" /></div>
           <span className="text-gray-500 text-[12px] lg:text-sm">Manual</span>
         </div>
         <div className="flex items-center gap-x-1">
-          <div className="w-4 lg:w-6 h-4 lg:h-6"><img className="w-full object-cover" src="/icons/People.svg" alt="icon-People" /></div>
+          <div className="w-4 lg:w-5 h-4 lg:h-5"><img className="w-full object-cover" src="/icons/People.svg" alt="icon-People" /></div>
           <span className="text-gray-500 text-[12px] lg:text-sm">{data.anyPeople}People</span>
         </div>
       </div>
       <div className="w-full flex justify-between items-center">
-        <div>
-          <span className="lg:text-xl flex items-center font-bold dark:text-white">${data.price}/<p className="text-[12px] text-gray-500">day</p></span>
-        </div>
+        <span className="lg:text-xl select-none flex items-center font-bold dark:text-white">${data.price}/<p className="text-[12px] text-gray-500">day</p></span>
         <Link to="/" className="hover:scale-95 outline-none duration-300 text-[12px] py-3 px-4 rounded lg:text-base text-white bg-blue-500">Rent Now</Link>
       </div>
     </div >
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,13 +1,19 @@
-import { useEffect, useState } from "react";
-import { FiChevronDown, FiFilter } from "react-icons/fi"
+import { Backdrop, backdropClasses } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import { FiChevronDown, FiChevronUp, FiFilter } from "react-icons/fi"
 import ProductCard from "../components/ProductCard";
 import { server } from "../server/server";
 
 const SearchPage = () => {
 
+  const filter = useRef()
+  const backDrop = useRef()
+  const [openFilter, setOpenFilter] = useState(false)
   const [cars, setCars] = useState(null)
   const [data, setData] = useState({
     sort: "Best Seller",
+    type: "sport",
+    people: "8 any",
   })
 
   const sortOptions = [
@@ -21,36 +27,64 @@ const SearchPage = () => {
     setCars(server)
   }, [])
 
+  const handleSelectedFilter = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
+  }
+
+  const handleFilterMobile = () => {
+    const filterClasses = [...filter.current.classList]
+
+    if (filterClasses.includes("top-[calc(100%-55px)]")) {
+      filter.current.classList.replace("top-[calc(100%-55px)]", "bottom-0")
+      backDrop.current.classList.replace("top-full", "top-0")
+      setOpenFilter(true)
+
+    } else {
+      filter.current.classList.replace("bottom-0", "top-[calc(100%-55px)]")
+      backDrop.current.classList.replace("top-0", "top-full")
+      setOpenFilter(false)
+    }
+  }
+
   return (
     <section className="w-full container grid grid-cols-1 grid-rows-[60px,1fr] md:grid-cols-6 lg:grid-cols-12 gap-3 md:py-4">
+      {/* bakDrop */}
+      <span ref={backDrop} className="fixed top-full md:hidden h-full duration-300 w-full z-[5] bg-zinc-800/70"></span>
+
       {/* Filter */}
-      <article className="hidden min-h-full md:block col-span-1 md:row-span-full md:col-span-2 lg:col-span-3">
+      <article ref={filter} className="fixed z-10 top-[calc(100%-55px)] border-t-2 border-blue-500 duration-300 w-full rounded-t-xl md:border-t-0 md:rounded-none md:min-h-full bg-white md:block md:bg-transparent md:static md:row-span-full md:col-span-2 lg:col-span-3 xl:col-span-2">
         <div className="text-slate-400 w-full md:sticky top-4 p-4 flex flex-col gap-y-4 md:bg-white md:dark:bg-zinc-800 rounded-lg md:shadow">
+          <div className="w-full md:hidden flex justify-center items-center ">
+            <button onClick={handleFilterMobile} className={`${openFilter === true ? "text-red-500" : "text-blue-500"} flex justify-center items-center gap-x-1`}>
+              {openFilter === true ? "close" : "more"} Filter
+              <FiChevronUp className={`text-xl duration-300 ${openFilter === true ? "rotate-180" : "rotate-0"}`} />
+            </button>
+          </div>
+          {/* type */}
           <div className="w-full flex flex-col gap-y-1">
             <span className="text-blue-500 select-none mb-1 flex justify-between w-full">Type<FiChevronDown className="text-xl" /></span>
-            <span className="cursor-pointer text-sm pl-2">sport</span>
-            <span className="cursor-pointer text-sm pl-2">classic</span>
-            <span className="cursor-pointer text-sm pl-2">sport</span>
-            <span className="cursor-pointer text-sm pl-2">classic</span>
+            <button onClick={e => handleSelectedFilter(e)} name="type" value="sport" className={`${data.type === "sport" && "text-green-500"} text-left cursor-pointer text-sm pl-2`}>sport</button>
+            <button onClick={e => handleSelectedFilter(e)} name="type" value="classic" className={`${data.type === "classic" && "text-green-500"} text-left cursor-pointer text-sm pl-2`}>classic</button>
           </div>
+          {/* people */}
           <div className="w-full flex flex-col gap-y-1">
             <span className="text-blue-500 select-none mb-1 flex justify-between w-full">people<FiChevronDown className="text-xl" /></span>
-            <span className="cursor-pointer text-sm pl-2">2 any</span>
-            <span className="cursor-pointer text-sm pl-2">3 any</span>
-            <span className="cursor-pointer text-sm pl-2">4 any</span>
-            <span className="cursor-pointer text-sm pl-2">8 any</span>
+            <button onClick={e => handleSelectedFilter(e)} name="people" value="2 any" className={`${data.people === "2 any" && "text-green-500"} text-left cursor-pointer text-sm pl-2`}>2 any</button>
+            <button onClick={e => handleSelectedFilter(e)} name="people" value="3 any" className={`${data.people === "3 any" && "text-green-500"} text-left cursor-pointer text-sm pl-2`}>3 any</button>
+            <button onClick={e => handleSelectedFilter(e)} name="people" value="4 any" className={`${data.people === "4 any" && "text-green-500"} text-left cursor-pointer text-sm pl-2`}>4 any</button>
+            <button onClick={e => handleSelectedFilter(e)} name="people" value="8 any" className={`${data.people === "8 any" && "text-green-500"} text-left cursor-pointer text-sm pl-2`}>8 any</button>
           </div>
         </div>
       </article>
 
       {/* Sort */}
-      <article className="col-span-1 whitespace-nowrap row-span-1 md:col-span-4 lg:col-span-9 text-sm lg:text-base">
+      <article className="col-span-1 whitespace-nowrap row-span-1 md:col-span-4 lg:col-span-9 xl:col-span-10 text-sm lg:text-base">
         <ul className="min-w-full h-full overflow-x-scroll px-4 text-slate-400 flex items-center justify-end gap-x-2 md:bg-white md:dark:bg-zinc-800 rounded-lg md:shadow">
           {sortOptions.map(item => {
             return (
               <li
                 onClick={e => setData({ ...data, sort: item.value })}
-                className={`${data.sort === item.value ? "border-blue-500 text-blue-500" : "border-slate-400"} bg-white dark:bg-zinc-800 duration-300 border rounded-full md:rounded-none md:border-0 py-1.5 px-3 cursor-pointer`}>
+                className={`${data.sort === item.value ? "border-green-500 text-green-500" : "border-slate-400"} select-none bg-white dark:bg-zinc-800 duration-300 border rounded-full md:rounded-none md:border-0 py-1.5 px-3 cursor-pointer`}>
                 {item.title}
               </li>
             )
@@ -61,8 +95,9 @@ const SearchPage = () => {
       </article>
 
       {/* Products */}
-      <article className="col-span-1 row-span-2 md:col-span-4 lg:col-span-9 px-4 md:px-0 bg-transparent">
-        <div className="w-full h-full gap-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 rounded-lg">
+      <article className="col-span-1 row-span-2 md:col-span-4 lg:col-span-9 px-4 md:px-0 xl:col-span-10 bg-transparent">
+        <div className="w-full h-full gap-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 rounded-lg">
+          {cars ? cars.map(car => <ProductCard key={car.id} data={car} />) : "Loading..."}
           {cars ? cars.map(car => <ProductCard key={car.id} data={car} />) : "Loading..."}
         </div>
       </article>
