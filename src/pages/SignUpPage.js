4370a9ad83cf5fdaa0f294bf6053f3ab51fd9dcd
mahdi bashiri <mahdi94250@gmail.com>
2023-01-31 07:38:29 +0330

    add page sign up

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -7,32 +7,52 @@ import * as Yup from "yup"
 import CheckBox from "../components/common/CheckBox";
 import InputCustom from "../components/common/Input.";
 
-const SignInPage = () => {
+const SignUpPage = () => {
 
   const [showPassword, setShowPassword] = useState(false)
 
   const formik = useFormik({
     initialValues: {
       name: "",
+      phoneNumber: "",
       email: "",
       password: "",
+      passwordConfirmation: "",
     },
     validationSchema: Yup.object().shape({
+      name: Yup.string("").max(15, "max length 15 elements").required("is valid"),
+      phoneNumber: Yup.string("").min(11, "phone number is valid").max(11, "phone number is valid").required("is valid"),
       email: Yup.string("").email("type email is valid").required("is valid"),
       password: Yup.string("").required("is valid"),
+      passwordConfirmation: Yup.string("").required("is valid").oneOf([Yup.ref("password"), null], "is not matched password"),
     }),
     onSubmit: (value) => { console.log(value); },
     validateOnMount: true,
   })
 
   return (
-    <section className="container w-full grid grid-cols-1 md:grid-cols-2 md:py-12">
-      <article className="col-span-1 md:bg-white md:shadow md:dark:bg-zinc-800 px-5 py-20 md:rounded-l-lg flex items-center justify-center">
+    <section className="container w-full grid grid-cols-1 md:grid-cols-2 md:py-4">
+      <article className="col-span-1 md:bg-white md:shadow md:dark:bg-zinc-800 px-5 py-10 md:rounded-l-lg flex items-center justify-center">
         <form onSubmit={formik.handleSubmit} className="flex flex-col w-full max-w-[300px]">
+          <InputCustom
+            label={"Name"}
+            id={"name-sign-up"}
+            formik={formik}
+            placeholder="mahdi"
+            name={"name"}
+          />
+          <InputCustom
+            inputMode="numerical"
+            label={"Phone Number"}
+            id={"phone-number-sign-up"}
+            formik={formik}
+            placeholder="09302565157"
+            name={"phoneNumber"}
+          />
           <InputCustom
             type="email"
             label={"Email"}
-            id={"email-sign-in"}
+            id={"email-sign-up"}
             formik={formik}
             placeholder="morent@rent.io"
             name={"email"}
@@ -41,17 +61,29 @@ const SignInPage = () => {
             <InputCustom
               type={showPassword ? "text" : "password"}
               label={"Password"}
-              id={"password-sign-in"}
+              id={"password-sign-up"}
               formik={formik}
               placeholder="Morent123@xcd"
               name={"password"}
             />
             {showPassword ? <FiEye onClick={() => setShowPassword(false)} className="text-slate-600 dark:text-slate-800 text-xl absolute top-[45%] right-4 cursor-pointer" />
-              : <FiEyeOff onClick={() => setShowPassword(true)} className="text-slate-600 dark:text-slate-800 text-xl absolute top-[45%] right-4 cursor-pointer" />}
+              : <FiEyeOff onClick={() => setShowPassword(true)} className="text-slate-600 dark:text-slate-800 text-xl absolute top-[43%] right-4 cursor-pointer" />}
+          </div>
+          <div className="w-full relative">
+            <InputCustom
+              type={showPassword ? "text" : "password"}
+              label={"Password Confirm"}
+              id={"password-confirm-sign-up"}
+              formik={formik}
+              placeholder="Morent123@xcd"
+              name={"passwordConfirmation"}
+            />
+            {showPassword ? <FiEye onClick={() => setShowPassword(false)} className="text-slate-600 dark:text-slate-800 text-xl absolute top-[45%] right-4 cursor-pointer" />
+              : <FiEyeOff onClick={() => setShowPassword(true)} className="text-slate-600 dark:text-slate-800 text-xl absolute top-[43%] right-4 cursor-pointer" />}
           </div>
           <Button style={{ paddingTop: "0.6rem", paddingBottom: "0.6rem", borderRadius: "0.5rem", marginTop: "1rem" }} disabled={!formik.isValid} variant="contained" type="submit" color="primary">Sign In</Button>
           <span className="flex justify-center w-full mt-4">
-            <Link to="/sign-up" className="text-blue-500 text-sm">Create new account</Link>
+            <Link to="/sign-in" className="text-blue-500 text-sm">Login account</Link>
           </span>
         </form>
       </article>
@@ -63,4 +95,4 @@ const SignInPage = () => {
   );
 }
 
-export default SignInPage;
+export default SignUpPage;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,6 +6,7 @@ import NotFoundPage from "../pages/NotFoundPage";
 import RentalPage from "../pages/RentalPage";
 import SearchPage from "../pages/SearchPage";
 import SignInPage from "../pages/SignInPage";
+import SignUpPage from "../pages/SignUpPage";
 
 
 const routes = [
@@ -13,6 +14,7 @@ const routes = [
   { id: 2, path: "/likes", element: <LikesPage /> },
   { id: 3, path: "/cars", element: <SearchPage /> },
   { id: 4, path: "/sign-in", element: <SignInPage /> },
+  { id: 4, path: "/sign-up", element: <SignUpPage /> },
   { id: 5, path: "/rental/:name/:id", element: <RentalPage /> },
   { id: 6, path: "/car/:name/:id", element: <DetailCarPage /> },
   { id: 7, path: "*", element: <NotFoundPage /> },
