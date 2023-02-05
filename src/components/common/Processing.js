e7780dce0e854e699ee1cbdbe0f1341ac26589d6
mahdi bashiri <mahdi94250@gmail.com>
2023-02-05 12:15:52 +0330

    edited style component processing in dark mode

--- src/components/common/Processing.js
+++ src/components/common/Processing.js
@@ -1,8 +1,8 @@
 const Processing = ({ loading }) => {
   return (
-    loading && <aside className="w-screen h-screen fixed top-0 left-0 z-10 bg-zinc-800/80 dark:bg-gray-100/60 flex items-center justify-center">
-      <div className="rounded-[10px] flex flex-col items-center gap-y-4 shadow bg-white dark:bg-zinc-800 dark:text-white p-10">
-        <span className="animate-spin w-8 h-8 border-4 border-slate-800 dark:border-wight rounded-full border-l-transparent block"></span>
+    loading && <aside className="w-screen h-screen fixed top-0 left-0 z-10 bg-zinc-800/80 dark:bg-zinc-700/80 flex items-center justify-center">
+      <div className="rounded-[10px] select-none flex flex-col items-center gap-y-4 shadow bg-white dark:bg-zinc-800 dark:text-white p-10">
+        <span className="animate-spin w-8 h-8 border-4 border-slate-800 dark:border-gray-400 dark:border-l-transparent rounded-full border-l-transparent block"></span>
         Processing ...
       </div>
     </aside>
